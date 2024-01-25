d779df6397706401f72ca8f3165c9c907f9ccd26
Panchanan Panigrahi <panchanandevops@gmail.com>
2024-01-26 02:50:12 +0530

    comments are added

--- api/config.go
+++ api/config.go
@@ -7,28 +7,41 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// envACCOUNTSID retrieves the Twilio Account SID from the environment variables.
 func envACCOUNTSID() string {
+
+	// Unmarshal .env file
 	println(godotenv.Unmarshal(".env"))
+
+	// Load and return Twilio Account SID from .env
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalln(err)
 		log.Fatal("Error loading .env file")
 	}
+
 	return os.Getenv("TWILIO_ACCOUNT_SID")
 }
 
+// envAUTHTOKEN retrieves the Twilio Auth Token from the environment variables.
 func envAUTHTOKEN() string {
+
+	// Load and return Twilio Auth Token from .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	return os.Getenv("TWILIO_AUTHTOKEN")
 }
 
+// envSERVICESID retrieves the Twilio Services ID from the environment variables.
 func envSERVICESID() string {
+
+	// Load and return Twilio Services ID from .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	return os.Getenv("TWILIO_SERVICES_ID")
 }
--- api/handler.go
+++ api/handler.go
@@ -6,48 +6,60 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/panchanandevops/Twilio-OTP-Verification/data"
-
 	"github.com/gin-gonic/gin"
+	"github.com/panchanandevops/Twilio-OTP-Verification/data"
 )
 
 const appTimeout = time.Second * 10
 
+// sendSMS is a Gin middleware function that handles the sending of SMS with OTP.
 func (app *Config) sendSMS() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		// Create a context with timeout
 		_, cancel := context.WithTimeout(context.Background(), appTimeout)
-		var payload data.OTPData
 		defer cancel()
 
+		// Initialize payload to hold request data
+		var payload data.OTPData
 		app.validateBody(c, &payload)
 
+		// Create a new data structure with sanitized input
 		newData := data.OTPData{
 			PhoneNumber: payload.PhoneNumber,
 		}
 
+		// Call Twilio service to send OTP
 		_, err := app.twilioSendOTP(newData.PhoneNumber)
 		if err != nil {
 			app.errorJSON(c, err)
 			return
 		}
 
+		// Respond with success message
 		app.writeJSON(c, http.StatusAccepted, "OTP sent successfully")
 	}
 }
 
+// verifySMS is a Gin middleware function that handles the verification of SMS OTP.
 func (app *Config) verifySMS() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		// Create a context with timeout
 		_, cancel := context.WithTimeout(context.Background(), appTimeout)
-		var payload data.VerifyData
 		defer cancel()
 
+		// Initialize payload to hold request data
+		var payload data.VerifyData
 		app.validateBody(c, &payload)
 
+		// Create a new data structure with sanitized input
 		newData := data.VerifyData{
 			User: payload.User,
 			Code: payload.Code,
 		}
 
+		// Call Twilio service to verify OTP
 		err := app.twilioVerifyOTP(newData.User.PhoneNumber, newData.Code)
 		fmt.Println("err: ", err)
 		if err != nil {
@@ -55,6 +67,7 @@ func (app *Config) verifySMS() gin.HandlerFunc {
 			return
 		}
 
+		// Respond with success message
 		app.writeJSON(c, http.StatusAccepted, "OTP verified successfully")
 	}
 }
